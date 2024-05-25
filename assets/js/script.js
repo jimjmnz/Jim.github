35c031ca46c55b4800c6a6afda9edaa14c8f80df
jimjmnz <78629755+jimjmnz@users.noreply.github.com>
2024-05-25 10:39:14 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Replace 'download.php' with the path to your server-side script
+    window.location = '../assets/js/download.php';
+});
