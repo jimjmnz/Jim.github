ec55fd3012fecbd4b20b8f881551099a6fd58135
jimjmnz <78629755+jimjmnz@users.noreply.github.com>
2024-05-25 11:32:12 +0800

    Update script.js
    Signed-off-by: jimjmnz <78629755+jimjmnz@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = './assets/js/Jimenez_Resume.pdf'; // Ensure this path is correct
+    link.href = 'Jimenez_Resume.pdf'; // Ensure this path is correct
     link.download = 'Jimenez_Resume.pdf'; // This will set the download attribute
     document.body.appendChild(link); // Append to the body to make it part of the document
     link.click(); // Trigger the download
