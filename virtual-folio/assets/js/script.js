1368f282ff2045acbc1bc8486286845ec57b601a
jimjmnz <78629755+jimjmnz@users.noreply.github.com>
2024-05-25 10:35:08 +0800

    Delete virtual-folio directory

--- virtual-folio/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    // Replace 'download.php' with the path to your server-side script
-    window.location = '../assets/js/download.php';
-});
